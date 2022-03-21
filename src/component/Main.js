b187466835c4a71ee0f76dcfd7058784ce219790
Sangeeth Raj <sjack7775@gmail.com>
2022-03-21 23:30:06 +0530

    update on home page

---
+++ src/component/Main.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import Nav from "./Nav";
+
+let API_KEY = "f5baf8c74c7d5f00a242c165979d0913"
+let base_url = "https://api.themoviedb.org/3"
+let sub_url = "/trending/movie/week?api_key="
+let url = base_url + sub_url + API_KEY;
+// let url = "https://api.themoviedb.org/3/trending/movie/week?api_key="+API_KEY;
+console.log(process.env);
+
+const Main = () => {
+    const [movieData, setData] = useState([]);
+    const [url_set, setUrl] = useState(url);
+
+    useEffect(() => {
+        fetch(url_set).then(res => res.json()).then(data => {
+            setData(data.results);
+        });
+    }, [url_set])
+
+    return (
+        <>
+            <div className="App">
+                <header className="Movie-App">
+                </header>
+                <body>
+                    <Nav />
+                    <div className="container">
+                        <h3>Trending Movies</h3>
+                    </div>
+                    <div className="row">
+                        {
+                            (movieData.length == 0) ? <p>Not Found</p> : movieData.map((res, pos) => {
+                                return (
+                                    <Card info={res} key={pos} />
+                                )
+                            })
+                        }
+                    </div>
+                </body>
+            </div>
+        </>
+    );
+}
+export default Main;
---
+++ src/component/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav>
+            <div class="nav-wrapper">
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="" className="home">Home</a></li>
+                    <li><a href="" className="upcoming">Upcoming Movies</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
